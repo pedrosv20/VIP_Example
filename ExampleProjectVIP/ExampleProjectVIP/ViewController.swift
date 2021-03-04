f3becaf268ccedc504d0de0aac6aa8c2d57fd97a
Pedro Vargas <pedro.vargas@sigga.com>
2021-03-04 09:48:16 -0300

    Create initial project

---
+++ ExampleProjectVIP/ExampleProjectVIP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleProjectVIP
+//
+//  Created by Pedro Vargas on 04/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
