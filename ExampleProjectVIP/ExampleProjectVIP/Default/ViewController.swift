a5cee1d6f2e7507d3f3f496f3887d6cd25f13eb8
Pedro Vargas <pedro1silva1vargas@gmail.com>
2021-03-04 11:12:50 -0300

    Create CodedView openClass an inherits it in ViewController to cretate initial view (#3)
    Co-authored-by: Pedro Vargas <pedro.vargas@sigga.com>

--- ExampleProjectVIP/ExampleProjectVIP/Default/ViewController.swift
+++ ExampleProjectVIP/ExampleProjectVIP/Default/ViewController.swift
@@ -1,10 +1,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func loadView() {
+        view = BackgroundRedView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("oi")
     }
 
 
--- ExampleProjectVIP/ExampleProjectVIP/Scenes/BackgroundRedScene/View/BackgroundRedView.swift
+++ ExampleProjectVIP/ExampleProjectVIP/Scenes/BackgroundRedScene/View/BackgroundRedView.swift
@@ -1,3 +1,45 @@
-import Foundation
+import UIKit
 
-final class BackgroundRedView {}
+final class BackgroundRedView: CodedView {
+    // MARK: - UIComponents
+
+    private lazy var teste: UIView = {
+      let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    // MARK: - Initialization
+
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Layout Setup
+
+    override public func addSubViews() {
+        self.addSubview(teste)
+    }
+    
+    override public func constrainSubViews() {
+        constraintTeste()
+        
+    }
+    
+    // MARK: - Constraint Setup
+
+    private func constraintTeste() {
+        teste.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            teste.topAnchor.constraint(equalTo: self.topAnchor),
+            teste.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            teste.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            teste.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+        ])
+    }
+}
